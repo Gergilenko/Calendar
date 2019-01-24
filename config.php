5a438cf22f5544052b218d0a6cd4dc527dc2ade7
Yuri <gergilenko@gmail.com>
2019-01-24 03:54:59 +0200

    changed credentials

--- config.php
+++ config.php
@@ -1,16 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Yuriy
- * Date: 26.01.2016
- * Time: 22:05
- */
 
 // Set MySQL connection parameters here
 
 define('DB_HOST', 'localhost');
-define('DB_USER', 'netpeak');
-define('DB_PASSWORD', 'netpeak');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
 define('DB_BASE', 'calendar');
 
 define('DATE_PATTERN', "/^(20[0-2][0-9])-([0-1][0-9])-([0-3][0-9])$/");
